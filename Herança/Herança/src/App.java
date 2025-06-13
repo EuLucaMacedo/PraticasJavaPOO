3f441477795dac5dda55ed515b506bf2f483dde5
Luca Macedo <lucamacedo.lm@gmail.com>
2025-06-12 22:32:51 -0300

    Desenvolvendo um Codigo com Herança Pessoa/Aluno

---
+++ Herança/Herança/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Aluno a = new Aluno();
+
+        a.setNome("Luca");
+        a.setIdade(27);
+        a.setSexo("Masc");
+        a.setMatricula(1621);
+        a.setCurso("Engenharia de Software");
+        
+
+
+
+    }
+}
